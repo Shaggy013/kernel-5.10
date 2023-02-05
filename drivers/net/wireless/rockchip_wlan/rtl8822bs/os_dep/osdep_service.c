bb5fae19307a1525791f89bb47eee2af6e432c72
Shaggy013 <thc013@ziggo.nl>
2023-02-05 19:10:46 +0100

    gentoo 1

--- drivers/net/wireless/rockchip_wlan/rtl8822bs/os_dep/osdep_service.c
+++ drivers/net/wireless/rockchip_wlan/rtl8822bs/os_dep/osdep_service.c
@@ -27,7 +27,6 @@ atomic_t _malloc_size = ATOMIC_INIT(0);
 #endif
 #endif /* DBG_MEMORY_LEAK */
 
-MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
 
 #if defined(PLATFORM_LINUX)
 /*
